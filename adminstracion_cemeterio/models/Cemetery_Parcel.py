142f60db56f23ad6e6ede853c1100f111005a8d3
Raziel101 <81720812+Raziel101@users.noreply.github.com>
2025-01-23 09:11:18 -0300

    Add files via upload

---
+++ adminstracion_cemeterio/models/Cemetery_Parcel.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+
+class CemeteryParcel(models.Model):
+    _name = 'cemetery.parcel'
+    _description = 'Parcela del Cementerio'
+
+    """toggle_option = fields.Selection([
+        ('nicho', 'Nicho'),
+        ('boveda', 'Boveda'),
+        ('panteon', 'Panteon'),
+        ('nichera', 'Nichera')
+    ], string='Seleccionar Opción', required=True, default='nicho')"""
+    panteon = fields.Boolean(string="Panteon")
+    fosa = fields.Boolean(string="Fosa")
+    nichera = fields.Boolean(string="Nichera")
+    boveda = fields.Boolean(string="Boveda")
+
+
+    #Nicho,Nichera y Panteon
+    galeria = fields.Integer(string='Galería')
+    fila = fields.Integer(string='Fila')
+    nicho = fields.Integer(string='Nicho')
+
+    #Bobeda
+    lote = fields.Integer(string="Lote")
+    manzana = fields.Integer(string='Manzana')
+    solar = fields.Integer(string='Solar')
+
+    representante_id = fields.Many2one('cemetery.representative', string="Representante")
+
+    death_ids = fields.Many2many(
+        'cemetery.death',
+        'cemetery_death_parcel_rel',
+        'parcel_id',
+        'death_id',
+        string="Fallecido"
+    )
+
+    sociedad_italiana = fields.Boolean(string="¿Es parte de la Sociedad Ialiana?", default=False)
+
+    contract_years = fields.Selection([(str(i), str(i)) for i in range(1, 100)], string='Años Contratados')
+    contract_start_date = fields.Date(string='Fecha de Arrendamiento')
+    contract_end_date = fields.Date(string='Fecha de Vencimiento')
+
+
+
+
+
---
+++ adminstracion_cemeterio/models/Cemetery_Representative.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class CemeteryRepresentavie(models.Model):
+    _name = 'cemetery.representative'
+    _description = 'Representante '
+
+    # Datos Personales
+    name = fields.Char(string='Nombre y Apellido', required=True)
+    domicilio = fields.Char(string='Domicilio', required=True)
+    fecha_nacimiento = fields.Date(string='Fecha de Nacimiento')
+    telefono = fields.Char(string='Teléfono')
+    correo_electronico = fields.Char(string='Correo Electrónico')
+    dni = fields.Char(string='DNI')
+    observaciones = fields.Char(string="Observaciones")
+
+    death_ids = fields.One2many('cemetery.death', 'representante_id', string='Fallecidos')
+    parcel_ids = fields.One2many('cemetery.parcel', 'representante_id', string='Parcelas')
